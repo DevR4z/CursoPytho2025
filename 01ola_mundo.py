e35641c0927c67cb08259649fee5831c4d9456a7
Rafael F Abranches <rf00101@gmail.com>
2025-03-26 19:17:15 -0300

    Calculadora

--- 01ola_mundo.py
+++ 01ola_mundo.py
@@ -4,3 +4,4 @@ print("Olá mundo!! Dessa vez vai!")
 # %%
 # esse é outro comentário
 print("Que dia bom!")
+print("Meu nome é Rafael!!")
---
+++ 02calculadora.py
@@ -0,0 +1,37 @@
+# %%
+
+# SOMA
+print("1 + 1=", 1+1)
+
+# %%
+
+# SUBTRAÇÃO
+print("10 - 5 =", 10-5)
+
+# %%
+
+# MULTIPLICAÇÃO
+print("10 x 5 =", 10*5)
+
+# %%
+
+# Divisão
+print("10 / 3 =", 10/3)
+
+# %%
+
+# Divisão com inteira parte
+print("10 // 3 =", 10//3)
+
+# %%
+
+# Resto da divisão
+print("10 % 3 =", 10 % 3)
+
+# %%
+
+# Potencia
+print("3 ** 4=", 3**4)
+# %%
+
+print("Meu nome é:", "Rafael")
