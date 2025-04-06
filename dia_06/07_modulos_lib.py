64b5046ffb7e2793ae3f64398f1507be09e7e3ab
Rafael F Abranches <rf00101@gmail.com>
2025-04-06 19:01:59 -0300

    dia 07

---
+++ dia_06/07_modulos_lib.py
@@ -0,0 +1,15 @@
+# %%
+import math
+from math import pi, e
+from math import *
+
+import math as mh
+# %%
+mh.factorial(4)
+
+math.sqrt(9)
+
+# %%
+math.pow(2, 4) # 2 elevado a 4
+
+# %%
---
+++ dia_06/08_aleatorio.py
@@ -0,0 +1,10 @@
+# %%
+import random
+
+# %%
+random.randint(1, 10)
+
+# %%
+
+x = ["Téo", "Maria", "Jose", "Ana", "João", "Rafael"]
+random.choice(x)
